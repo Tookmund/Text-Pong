ecae462f1b636ef419e84e97043e7bae2ba6f5bd
Jacob Adams <tookmund@gmail.com>
2015-06-30 22:28:28 -0400

    Give instructions

--- main.java
+++ main.java
@@ -9,6 +9,15 @@ public class main
       {
          System.err.println("Warning: This game was was designed to run in cmd.exe.\nYour play experience may be suboptimal.");
       }
+      System.out.println("Player 1 uses W and S to move the paddle\nPlayer 2 uses I and K to move the paddle\nUnfortunately I cannot uncook a terminal in Java so you must press enter for the game to continue\nReady? ");
+      try 
+      {
+         System.in.read();
+      }
+      catch(Exception e)
+      {
+      
+      }
       Scanner input = new Scanner(System.in);
       // Windows console is 22x78
       game Game = new game(22,78);
